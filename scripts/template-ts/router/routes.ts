3d1f22e8a7df4e9acd0913040817fdaba4e23aff
abc <3109952@qq.com>
2024-03-12 15:44:35 +0800

    by jc

--- scripts/template-ts/router/routes.ts
+++ scripts/template-ts/router/routes.ts
@@ -3,7 +3,7 @@ export const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'index',
-    component: () => import('@Project/项目名称/views/test/test.vue'),
+    component: () => import('@Project/项目名称/views/home/home.vue'),
     meta: { title: '首页' }
   }
 ]
--- scripts/template/router/index.js
+++ scripts/template/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'index',
-      component: () => import('@Project/项目名称/views/test/test.vue'),
+      component: () => import('@Project/项目名称/views/home/home.vue'),
       meta: { title: '首页' }
     }
   ]
